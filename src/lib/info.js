12afcd1e44505c558a57d96d7b54da251f623a60
Alva <shailesh.alva@gmail.com>
2023-04-03 20:48:50 -0700

    Update the site information

--- src/lib/info.js
+++ src/lib/info.js
@@ -3,25 +3,12 @@
 // feel free to remove this file.
 
 // URL of your website
-export const website = 'https://sveltekit-blog-template.vercel.app'
+export const website = 'https://alvatech.io'
 
-export const firstName = 'Jordan'
-export const lastName = 'Doe'
-export const name = `${firstName} ${lastName}`
+export const firstName = 'AlvaTech'
+export const name = `${firstName}`
 
-// Used for the landing page & footer of posts
-export const avatar = 'https://api.lorem.space/image/face?w=250&h=250'
-export const bio = `\
-I’m ${firstName}, a software engineer based in Canada. Lorem ipsum dolor sit amet, consectetur
-adipiscing elit. Phasellus accumsan quam at eros malesuada, id luctus lectus efficitur. Nulla
-sit amet arcu mi. Duis quis mauris malesuada, posuere orci et, pulvinar nisl. Nam malesuada
-vehicula sapien vitae sagittis.
-`
-
-// Your social usernames, empty strings won't be used.
-// For sake of the live demo, I'm using `username/repo` for github,
-// but you can replace with just your username.
-export const github = 'mattjennings/sveltekit-blog-template'
+export const github = 'alvatech'
 export const twitter = ''
 export const linkedin = ''
 export const instagram = ''
--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { website, name, bio, avatar } from '$lib/info.js'
+  import { website, name } from '$lib/info.js'
   import ToC from '$lib/components/ToC.svelte'
   import ArrowLeftIcon from '$lib/components/ArrowLeftIcon.svelte'
   import SocialLinks from '$lib/components/SocialLinks.svelte'
@@ -96,18 +96,6 @@
         <div class="flex justify-center order-1 col-span-2 gap-6 md:order-2">
           <SocialLinks />
         </div>
-        <div class="flex justify-center order-2 md:order-1 md:col-span-2">
-          <a href="/" class="inline-block rounded-full">
-            <img
-              src={avatar}
-              alt={name}
-              class="w-24 h-24 mx-auto rounded-full md:w-28 md:h-28 ring-2 ring-zinc-200 dark:ring-zinc-700"
-            />
-          </a>
-        </div>
-        <p class="order-3 text-base text-zinc-600 dark:text-zinc-400">
-          {bio}
-        </p>
       </div>
     </div>
   </div>
