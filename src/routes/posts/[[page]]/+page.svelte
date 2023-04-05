9d0bec687e714bd8fdb4d36c0b809628c7be76be
Alva <shailesh.alva@gmail.com>
2023-04-05 15:22:57 -0700

    Delete the getting started page

--- src/routes/posts/[[page]]/+page.svelte
+++ src/routes/posts/[[page]]/+page.svelte
@@ -16,14 +16,7 @@
 </svelte:head>
 
 <div class="flex flex-col flex-grow">
-  <header class="pt-4">
-    <h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
-      Writing on tech, music, and whatever else I feel like
-    </h1>
-    <p class="mt-6">All of my written content collected in one place</p>
-  </header>
-
-  <div class="mt-16 sm:mt-20">
+  <div class="mt-2 sm:mt-4">
     <PostsList posts={data.posts} />
   </div>
 
